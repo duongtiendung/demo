f18367fd821a352bb247fb98592aba26a871bf0e
dungdt <dungdt@lifetimetech.vn>
2020-06-23 18:50:57 +0700

    commit code

--- main.go
+++ main.go
@@ -1,26 +1,22 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
 func main() {
 
-    fmt.Scanf("%s", &name)
-    
-    
-    var name string 
-    var alphabet_count int
-  
-    // Calling Scanf() function for 
-    // scanning and reading the input 
-    // texts given in standard input 
-    fmt.Scanf("%s", &name) 
-    fmt.Scanf("%d", &alphabet_count) 
-  
-    // Printing the given texts 
-    fmt.Printf("The word %s containg %d number of alphabets.",  
-               name, alphabet_count) 
-    
+	var name string
+	var alphabet_count int
+
+	// Calling Scanf() function for
+	// scanning and reading the input
+	// texts given in standard input
+	fmt.Scanf("%s", &name)
+	fmt.Scanf("%d", &alphabet_count)
+
+	// Printing the given texts
+	fmt.Printf("The word %s containg %d number of alphabets.",
+		name, alphabet_count)
 
 }
