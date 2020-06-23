0fa9491deceb617a5c1b32a9f1cfe288f3cdbcf0
duongtiendung <duongtiendung92@hotmail.com>
2020-06-23 18:44:40 +0700

    Update main.go

--- main.go
+++ main.go
@@ -6,6 +6,21 @@ import (
 
 func main() {
 
-    fmt.Println("Hello World")
+    fmt.Scanf("%s", &name)
+    
+    
+    var name string 
+    var alphabet_count int
+  
+    // Calling Scanf() function for 
+    // scanning and reading the input 
+    // texts given in standard input 
+    fmt.Scanf("%s", &name) 
+    fmt.Scanf("%d", &alphabet_count) 
+  
+    // Printing the given texts 
+    fmt.Printf("The word %s containg %d number of alphabets.",  
+               name, alphabet_count) 
+    
 
 }
